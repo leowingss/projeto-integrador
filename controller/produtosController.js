ac15cfa98ef675f2e90bb27f55d8126c61228399
Leandro <leandro@leandroleite.info>
2021-04-10 18:10:29 -0300

    sugestoes

--- controller/produtosController.js
+++ controller/produtosController.js
@@ -1,7 +1,7 @@
-const controlador = {
-    titulo: function(req, res, next) {
-    res.render('index', { title: 'E-commerce' });
-}
-}
+const constroller = {
+  home: function (req, res, next) {
+    res.render("index", { title: "E-commerce" });
+  },
+};
 
-module.exports = controlador;
+module.exports = constroller;
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,20 @@
-var express = require('express');
-var router = express.Router();
-const controlador = require("../controller/produtosController")
+const express = require("express");
+const router = express.Router();
+const controlador = require("../controller/produtosController");
 
 /* GET home page. */
-router.get('/', controlador.titulo);
+router.get("/", controlador.home);
 
-router.get('/promocoes', function(req, res, next) {
-  res.render('promocoes', { title: 'Promoções' });
+router.get("/promocoes", function (req, res, next) {
+  res.render("promocoes", { title: "Promoções" });
 });
-router.get('/login', function(req, res, next) {
-  res.render('login', { title: 'login' });
+
+router.get("/login", function (req, res, next) {
+  res.render("login", { title: "login" });
 });
-router.get('/cadastro', function(req, res, next) {
-  res.render('cadastro', { title: 'cadastro' });
+
+router.get("/cadastro", function (req, res, next) {
+  res.render("cadastro", { title: "cadastro" });
 });
+
 module.exports = router;
