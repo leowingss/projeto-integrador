14421e7801e132ff97988ff5e0a5751329904f7c
Itamar Souza <56474015+yekapetrovna@users.noreply.github.com>
2021-04-14 20:28:26 -0300

    edições e adições de itens

--- public/main.js
+++ public/main.js
@@ -7,3 +7,13 @@ $('.slider-principal').slick({
     autoplay: true,
     autoplaySpeed: 2000
   });
+
+  $('.slider-indoor').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,10 @@ const controlador = require("../controller/produtosController")
 /* GET home page. */
 router.get("/", controlador.home);
 
+router.get("/index", function (req, res, next) {
+  res.render("index", { title: "Home" });
+});
+
 router.get("/promocoes", function (req, res, next) {
   res.render("promocoes", { title: "Promoções" });
 });
@@ -16,5 +20,11 @@ router.get("/login", function (req, res, next) {
 router.get("/cadastro", function (req, res, next) {
   res.render("cadastro", { title: "cadastro" });
 });
+
+router.get("/indoor", function (req, res, next) {
+  res.render("indoor", { title: "Sports Indoor" });
+});
+
+
 
 module.exports = router;
