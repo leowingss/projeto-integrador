c7de21bc4869d88224210fc092ba3557abd02e75
Leonardo Santos Gonçales <leondani853@gmail.com>
2021-05-31 20:05:11 -0300

    alterando tabelas

--- docs/table.sql
+++ docs/table.sql
@@ -2,25 +2,12 @@ CREATE DATABASE IF NOT EXISTS quarentine_sports CHARSET utf8mb4;
 
 USE quarentine_sports;
 
-CREATE TABLE IF NOT EXISTS categorias ( 
-id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-nome VARCHAR(150) NOT NULL,
-slug VARCHAR(150) NOT NULL
-);
-
 CREATE TABLE IF NOT EXISTS produtos (
 id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
 nome VARCHAR(150),
 preco DECIMAL,
-image VARCHAR(500)
-);
-
-
-CREATE TABLE IF NOT EXISTS categorias_produtos (
-produto_id INT UNSIGNED NOT NULL ,
-categoria_id INT  UNSIGNED NOT NULL,
-FOREIGN KEY (produto_id) REFERENCES produtos(id),
-FOREIGN KEY (categoria_id) REFERENCES categorias(id)
+image VARCHAR(500),
+categoria VARCHAR(150)
 );
 
 CREATE TABLE IF NOT EXISTS usuarios ( 
@@ -31,7 +18,11 @@ senha VARCHAR (150) NOT NULL,
 endereco VARCHAR (150) NOT NULL
 );
 
-
-SELECT * FROM categorias_produtos;
-SELECT * FROM categorias;
-SELECT * FROM produtos;
+CREATE TABLE IF NOT EXISTS pedidos( 
+id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+data_pedido DATE, 
+usuario_id INT UNSIGNED NOT NULL,
+produto_id INT UNSIGNED NOT NULL,
+FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
+FOREIGN KEY (produto_id) REFERENCES produtos(id)
+);
