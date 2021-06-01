5b0a0a83fa5c8e234b00096b9887d4047575ce83
Leonardo Santos Gonçales <leondani853@gmail.com>
2021-06-01 20:10:15 -0300

    excluindo rota

--- src/controller/HomeController.js
+++ src/controller/HomeController.js
@@ -2,9 +2,6 @@ const HomeController = {
     home: function(req, res, next) {
         res.render("index", { title: "E-commerce" });
     },
-    destaque: (req, res, next) => {
-        res.render("destaques", { title: "Destaques" });
-    },
 
 };
 
