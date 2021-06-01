d4477592796ff0babcbe319777983942bdee17cd
Leonardo Santos Gonçales <leondani853@gmail.com>
2021-06-01 20:20:05 -0300

    testando produtos do banco de dados

--- src/controller/HomeController.js
+++ src/controller/HomeController.js
@@ -2,11 +2,7 @@ const HomeController = {
     home: function(req, res, next) {
         res.render("index", { title: "E-commerce" });
     },
-<<<<<<< HEAD
-    
-=======
 
->>>>>>> main
 };
 
 module.exports = HomeController;
