60526f2f7a6c15a265c08820192789bf97b3b654
Leonardo Santos Gonçales <leondani853@gmail.com>
2021-06-02 22:50:05 -0300

    alterando imagens e views

--- docs/table.sql
+++ docs/table.sql
@@ -88,5 +88,10 @@ INSERT INTO pedidos (id,data_pedido,usuario_id,produto_id) VALUES (1,"2021/06/01
 UPDATE produtos SET image = '/images/shorts.jpg' WHERE id = 23;
 UPDATE produtos SET image = '/images/tenis-nike.jpg' WHERE id = 26;
 UPDATE produtos SET image = '/images/calca-leggin-com-saia.jpg' WHERE id = 19;
+UPDATE produtos SET image = '/images/tenis-adidas.jpg' WHERE id = 25;
+UPDATE produtos SET image = '/images/tenis-asics.jpg' WHERE id = 27;
+UPDATE produtos SET image = '/images/top-fitness.jpg' WHERE id = 24;
+UPDATE produtos SET image = '/images/shorts-saia.jpg' WHERE id = 22;
+UPDATE produtos SET image = '/images/calca-legging.jpg' WHERE id = 20;
 
 -- UPDATE NAS IMAGENS
