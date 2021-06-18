4729aed019cebef249bb004dd68e9a4764f4b35c
Leonardo Santos Gonçales <leondani853@gmail.com>
2021-06-17 23:35:21 -0300

    feat: criptografando senha com hash

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -1,5 +1,6 @@
 const { Usuario } = require("../../database/models");
 const { validationResult } = require('express-validator')
+const bcrypt = require('bcrypt')
 
 const UserController = {
     create: (req, res) => {
@@ -7,6 +8,7 @@ const UserController = {
     },
     user: async(req, res) => {
         const { nome, email, senha } = req.body
+        const senhaHash = bcrypt.hashSync(senha, 10)
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             console.log(errors.mapped())
@@ -15,7 +17,7 @@ const UserController = {
         const result = await Usuario.create({
             nome,
             email,
-            senha
+            senha: senhaHash
         })
         console.log(result)
         return res.redirect('/')
