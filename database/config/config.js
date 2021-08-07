266f8bf2a9714503282d76374e16e32e32a7c19d
Leonardo Santos Gonçales <leondani853@gmail.com>
2021-08-07 12:59:34 -0300

    config database

--- database/config/config.js
+++ database/config/config.js
@@ -1,24 +1,23 @@
 module.exports = {
-  development: {
-    username: 'myuser',
-    password: 'mypass',
-    database: 'quarentine_sports',
-    host: '127.0.0.1',
-    port: 3307,
-    dialect: 'mysql'
-  },
-  test: {
-    username: 'root',
-    password: null,
-    database: 'database_test',
-    host: '127.0.0.1',
-    dialect: 'mysql'
-  },
-  production: {
-    username: 'root',
-    password: null,
-    database: 'database_production',
-    host: '127.0.0.1',
-    dialect: 'mysql'
-  }
+    development: {
+        username: 'root',
+        password: null,
+        database: 'quarentine_sports',
+        host: '127.0.0.1',
+        dialect: 'mysql'
+    },
+    test: {
+        username: 'root',
+        password: null,
+        database: 'database_test',
+        host: '127.0.0.1',
+        dialect: 'mysql'
+    },
+    production: {
+        username: 'root',
+        password: null,
+        database: 'database_production',
+        host: '127.0.0.1',
+        dialect: 'mysql'
+    }
 }
